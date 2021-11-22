f67fe035a5c77ebe935ea807779b110985b80c21
tduncan14 <nbatre1@yahoo.com>
2021-11-21 19:51:58 -0800

    keep moving forward

--- instagram/src/components/Screens/Login.js
+++ instagram/src/components/Screens/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import{Link} from 'react-router-dom'
 
 
 const Login = () => {
@@ -6,7 +7,25 @@ const Login = () => {
 
 
     return(
-        <h1>Login</h1>
+        
+        <div className="mycard ">
+      <div class="card  auth-card">
+          <h2 > <span className="heading1">Instagram </span></h2>
+          <input type="text" placeholder="email"/>
+
+          <input type="password" placeholder="password"/>
+
+
+          <button className="btn waves-effect waves-light light-blue lighten-2">
+              Login
+          </button>
+
+          <h5>
+              <Link to="/signin">Don't have an account?</Link>
+          </h5>
+
+      </div>
+      </div>
     )
 
 
--- instagram/src/components/Screens/Signup.js
+++ instagram/src/components/Screens/Signup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 
 
@@ -8,8 +9,30 @@ const Signup = () => {
 
 
     return (
-        <div>
-            Signup
+        <div className="mycard ">
+        <div class="card  auth-card">
+            <h2 > <span className="heading1">Instagram </span></h2>
+            <input type="text" placeholder="email"/>
+
+
+            
+            <input type="text" placeholder="name"/>
+  
+
+            <input type="password" placeholder="password"/>
+
+
+  
+  
+            <button className="btn waves-effect waves-light light-blue lighten-2">
+                Login
+            </button>
+
+            <h5>
+              <Link to="/signup"> Already have an account ?</Link>
+          </h5>
+  
+        </div>
         </div>
     )
 
