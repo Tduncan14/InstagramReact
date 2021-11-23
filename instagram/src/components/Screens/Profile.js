34c39967de684537fe0f32a20f19c7dcabf90659
tduncan14 <nbatre1@yahoo.com>
2021-11-22 17:16:37 -0800

    keep moving forward

--- instagram/src/components/Screens/Profile.js
+++ instagram/src/components/Screens/Profile.js
@@ -5,7 +5,39 @@ const Profile  = () => {
 
 
     return (
-        <h1>Login</h1>
+      <div>
+          <div style={{display:'flex', justifyContent:"space-around", margin:"18px 0px", borderBottom:"1px solid grey"}} >
+              <div>
+                  <img  style={{width:"160px",height:"160px",borderRadius:"50%"}}
+                  src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+              </div>
+              <div>
+                  <h4> Treek Lee</h4>
+                  <div style={{display:"flex",justifyContent:"space-between", width:"108%"}}>
+
+                      <h6> 40 posts</h6>
+                      <h6> 40 followers</h6>
+                      <h6> 40 following</h6>
+                  </div>
+              </div>
+         
+         
+         
+            <div className="gallery">
+
+             <img  className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+             <img  className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+             <img  className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+             <img  className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+             <img  className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+             <img  className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+             <img  className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+             <img  className="item" src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+             
+
+            </div>
+          </div>
+      </div>
     )
 
 
