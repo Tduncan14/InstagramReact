cb47b5b7501fa7bf792ff6e6a0e7dfe11d024c3b
tduncan14 <nbatre1@yahoo.com>
2021-11-24 22:42:12 -0800

    keep moving forward

---
+++ instagram/src/components/Screens/Createpost.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const CreatePost = () => {
+
+
+
+
+    return(
+        <div className="card input-filed">
+
+            <input type="text" placeholder="title"/>
+            <input type="text" placeholder="body"/>
+            
+
+        </div>
+    )
+
+
+
+}
+
+
+export default CreatePost
